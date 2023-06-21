0c33c53f7a87465d6036bf4c642f4164d80d8e89
izzat syamir <112875861+izzatsymr@users.noreply.github.com>
2023-06-21 12:24:08 +0800

    Update discussion-list-view.php

--- Views/General User/discussion-list-view.php
+++ Views/General User/discussion-list-view.php
@@ -13,31 +13,36 @@ mysqli_close($mysql);
 
 <!DOCTYPE html>
 <html>
+
 <head>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
-<body>
 
-<div class="container">
-  <h1>Question List</h1>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Question ID</th>
-        <th>User ID</th>
-        <th>Expert ID</th>
-        <th>Type</th>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Status</th>
-        <th>Remaining Time</th>
-        <th>Action</th>
-      </tr>
-    </thead>
-    <tbody>
-      <?php
-      // Loop through the result and display each question
-      while ($row = mysqli_fetch_assoc($result)) {
+<body>
+  <?php
+  include '..\..\Assets\navbar.html';
+  ?>
+
+  <div class="container">
+    <h1>Question List</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Question ID</th>
+          <th>User ID</th>
+          <th>Expert ID</th>
+          <th>Type</th>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th>Remaining Time</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+        // Loop through the result and display each question
+        while ($row = mysqli_fetch_assoc($result)) {
           echo "<tr>";
           echo "<td>" . $row['id'] . "</td>";
           echo "<td>" . $row['user_id'] . "</td>";
@@ -49,15 +54,16 @@ mysqli_close($mysql);
           echo "<td>" . $row['remaining_time'] . "</td>";
           echo "<td>";
           if ($row['expert_id'] === null) {
-              echo "<a href='discussion-edit-view.php?id=" . $row['id'] . "' class='btn btn-primary'>Edit</a>";
+            echo "<a href='discussion-edit-view.php?id=" . $row['id'] . "' class='btn btn-primary'>Edit</a>";
           }
           echo "</td>";
           echo "</tr>";
-      }
-      ?>
-    </tbody>
-  </table>
-</div>
+        }
+        ?>
+      </tbody>
+    </table>
+  </div>
 
 </body>
+
 </html>
